0eeeaba4b8dafbdcef00792995c644bd057f3c71
Acyyd <ptr.testus@gmail.com>
2024-02-03 17:46:27 -0500

    Minor form fix.

--- room_occupancy_module.py
+++ room_occupancy_module.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import f1_score,accuracy_score,ConfusionMatrixDisplay
+from sklearn.metrics import f1_score, accuracy_score, ConfusionMatrixDisplay
+
+
+####################################################################################
+############################ DATA EXTRACTION ########################################
+####################################################################################
 
 def read_file_df(file_path) :
     """ 
@@ -12,49 +18,61 @@ def read_file_df(file_path) :
     df  = pd.read_csv(file_path, compression='zip', parse_dates={"date": ["Date", "Time"]}, header=0, sep=',', quotechar='"')
     return df
 
+####################################################################################
+############################### PLOTTING ###########################################
+####################################################################################
+
 def plot_onecolumn_index_occupancy (df, column):
     """ 
     Plots the selected column vs index, with Room_Occupancy as the hue.
     """
-    fig,ax = plt.subplots(1,1,figsize = (15,5))
-    sns.scatterplot(data = df, x=df.index, y=column,hue = 'Room_Occupancy_Count', palette='coolwarm', edgecolor="none")
+    fig, ax = plt.subplots(1, 1,figsize = (15, 5))
+    sns.scatterplot(data=df, x=df.index, y=column, hue='Room_Occupancy_Count', palette='coolwarm', edgecolor="none")
     ax.set_xlabel('index')
 
 def plot_columns_index_occupancy (df, columns):
     """ 
     Plots the selected columns on separate graphs, sharing the same x (index), with Room_Occupancy as the hue.
     """
-    fig,axs = plt.subplots(len(columns),1,figsize = (15,7),sharex=True)
+    fig,axs = plt.subplots(len(columns), 1, figsize=(15, 7), sharex=True)
     fig.subplots_adjust(hspace=0)
-    for i,col in enumerate(columns) :
-        sns.scatterplot(data = df, x=df.index, y=col, ax=axs[i],hue = 'Room_Occupancy_Count', palette='coolwarm', edgecolor="none",legend=i==0)
+    for i, col in enumerate(columns):
+        sns.scatterplot(data=df, x=df.index, y=col, ax=axs[i], hue 'Room_Occupancy_Count', palette='coolwarm', edgecolor="none", legend=i==0)
     axs[-1].set_xlabel('index')
 
-def plot_one_columns_index_occupancy_compare_transformation (df, column,transformation):
+def plot_one_columns_index_occupancy_compare_transformation (df, column, transformation):
     """ 
     Plots the selected column before and after transformation on separate graphs. The dataframe index is used as x, with Room_Occupancy as the hue.
     """
-    _,axs = plt.subplots(1,2,figsize = (10,5))
-    sns.scatterplot(ax = axs[0],x = df.index,y = df[column], hue =df['Room_Occupancy_Count'],palette='coolwarm',edgecolor='none', legend=False)
-    sns.scatterplot(ax = axs[-1],x = df.index,y = transformation.fit_transform(df[column]), hue=df['Room_Occupancy_Count'],palette='coolwarm',edgecolor='none')
+    fig,axs = plt.subplots(1, 2, figsize=(10, 5))
+    sns.scatterplot(ax=axs[0], x=df.index, y=df[column], hue=df['Room_Occupancy_Count'], palette='coolwarm', edgecolor='none', legend=False)
+    sns.scatterplot(ax=axs[1], x=df.index, y=transformation.fit_transform(df[column]), hue=df['Room_Occupancy_Count'], palette='coolwarm', edgecolor='none')
+    axs[1].set_ylabel(f'window_averaged_{column}')
+    axs[0].set_xlabel('index')
+    axs[1].set_xlabel('index')
 
-def print_metrics(estimator,cross_validate):
+def print_metrics(estimator, cross_validate):
     """
-    Prints the min,mean and max values of Accuracy (A) and F1-score macro (F1) estimated with the given cross_validate procedure and estimator.
+    Prints the min, mean and max values of Accuracy (A) and F1-score macro (F1) estimated with the given cross_validate procedure and estimator.
     """
+    # calculate accuracy cross validation score
+    accuracy_scores = cross_validate['test_accuracy']
+    f1_macro_scores = cross_validate['test_f1_macro']
+
     print(estimator)
     print('metrics\t min\t mean\t max')
-    print('A', '\t',np.round(np.min(cross_validate['test_accuracy']),3),'\t', np.round(np.mean(cross_validate['test_accuracy']),3),'\t',np.round(np.max(cross_validate['test_accuracy']),3))
-    print('F1', '\t',np.round(np.min(cross_validate['test_f1_macro']),3),'\t', np.round(np.mean(cross_validate['test_f1_macro']),3),'\t',np.round(np.max(cross_validate['test_f1_macro']),3))
+    print('A', '\t', np.round(np.min(accuracy_scores),3),'\t', np.round(np.mean(accuracy_scores),3),'\t', np.round(np.max(accuracy_scores), 3))
+    print('F1', '\t', np.round(np.min(f1_macro_scores),3),'\t', np.round(np.mean(f1_macro_scores),3),'\t', np.round(np.max(f1_macro_scores), 3))
 
-def plot_confusion_acc_f1 (y_true,y_test,title):
+def plot_confusion_acc_f1 (y_true, y_test, title):
     """
     Plots the confusion matrix base on prediction with the given title.
     Also return accuracy and F1-score macro average.
     """
-    ConfusionMatrixDisplay.from_predictions(y_true,y_test)
+    ConfusionMatrixDisplay.from_predictions(y_true, y_test)
     plt.title(title)
-    plt.xlabel('Predicted label\n Accuracy = {:0.3f}  F1-macro = {:0.3f}'.format(accuracy_score(y_true,y_test),f1_score(y_true,y_test,average='macro')))
+    plt.xlabel('Predicted label\n Accuracy = {:0.3f}  F1-macro = {:0.3f}'\
+               .format(accuracy_score(y_true, y_test), f1_score(y_true, y_test, average='macro')))
     plt.plot();
 
 if __name__ == '__main__' :
--- transformers.py
+++ transformers.py
@@ -4,7 +4,8 @@ warnings.filterwarnings("ignore")
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 import numpy as np
-class Window_Average (BaseEstimator,TransformerMixin):
+
+class Window_Average (BaseEstimator, TransformerMixin):
     """
     Returns the averaged dataframe averaged using a rolling windows running along axis = 0 with the specified window size.
     """
@@ -17,7 +18,7 @@ class Window_Average (BaseEstimator,TransformerMixin):
 
     def transform(self, X, y=None):
         # transform only transforms the data
-        return X.rolling(self.window_size,min_periods=1,center=True).mean()
+        return X.rolling(self.window_size, min_periods=1, center=True).mean()
     
 class Window_Average_w_Edges (BaseEstimator,TransformerMixin):
     """
@@ -34,8 +35,12 @@ class Window_Average_w_Edges (BaseEstimator,TransformerMixin):
 
     def transform(self, X, y=None):
         # transform only transforms the data
-        new_df = X.copy()
-        index_group = pd.Series(np.cumsum(X.index.diff(1).fillna(1)!=1),index=X.index) #create a series containing the same number for groups of data with consecutive index    
+        # new_df = X.copy()
+        # index_group = pd.Series(np.cumsum(X.index.diff(1).fillna(1)!=1),index=X.index) #create a series containing the same number for groups of data with consecutive index    
+        # for i in index_group.unique():
+        #     new_df[index_group==i] = (X[index_group==i]).rolling(self.window_size,min_periods=1,center=True).mean()
+
+        index_group = pd.Series(np.cumsum(X.index.diff(1).fillna(1)!=1), index=X.index) #create a series containing the same number for groups of data with consecutive index    
         for i in index_group.unique():
-            new_df[index_group==i] = (X[index_group==i]).rolling(self.window_size,min_periods=1,center=True).mean()
-        return new_df
+            X[index_group==i] = (X[index_group==i]).rolling(self.window_size, min_periods=1, center=True).mean()
+        return X
